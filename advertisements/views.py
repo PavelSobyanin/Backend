a60d173b7ee351ed0ac366e8f5a396a386666d9a
Pavel Sobyanin <pavel_sobyanin_git@mail.ru>
2023-08-21 21:49:02 +0300

    Corrected

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,24 +1,30 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from .models import Advertisement
-from .serializers import AdvertisementSerializer
-from django_filters import rest_framework as filters
-from .filters import AdvertisementFilter
-from .permissions import IsAdvertisementOwner
+from rest_framework.permissions import IsAuthenticated
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.Permissions import IsOwnerPermission
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_class = AdvertisementFilter
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['creator', 'status', 'created_at']
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action == "create":
+        if self.action in ["create"]:
             return [IsAuthenticated()]
-        elif self.action in ["update", "partial_update", "delete"]:
-            return [IsAuthenticated(), IsAdvertisementOwner()]
+        elif self.action in ["update", "destroy", "partial_update"]:
+            return [IsOwnerPermission()]
         return []
 
